a5cf2278344d1b335986dfa9678bf081a6b6fb56
dlewis21837 <dlewis21@gmail.com>
2026-09-22 13:08:10 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,8 +2,7 @@ const CHANNEL = "SeekrTrending";
 const MAX_MARKET_CAP = 3_000_000;
 const MIN_LIQUIDITY = 10_000;
 const MIN_SCORE = 7;
-const TROJAN_BOT = "paris_trojanbot";
-const TROJAN_REF_CODE = "daniellew578683";
+
 const RAYDIUM_POOLS_URL = "https://api-v3.raydium.io/pools/info/list?poolType=all&poolSortField=default&sortType=desc&pageSize=100&page=1";
 const SOL_MINT = "So11111111111111111111111111111111111111112";
 const STABLE_MINTS = new Set([
